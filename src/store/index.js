9492ce53f70f25cc683f8f3ee4d11d8a54265d44
Caique Ferian <caiquegsf@gmail.com>
2022-04-14 14:34:51 -0300

    Adicionando projeto a um repositorio pessoal

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from '../reducers/index';
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import rootReducer from '../reducers/index';
+import { configureStore } from '@reduxjs/toolkit';
+import user from '../reducers/user';
+import wallet from '../reducers/wallet';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
+// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = configureStore({
+  reducer: {
+    user,
+    wallet,
+  },
+});
 export default store;
